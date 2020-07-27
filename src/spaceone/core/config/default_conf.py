456fbc85d8e235845d584c450279bf792711c7f5
Jongmin Kim <whdalsrnt@megazone.com>
2020-07-27 13:51:36 +0900

    Separate default config and config utils
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

---
+++ src/spaceone/core/config/default_conf.py
@@ -0,0 +1,15 @@
+# Service Configuration
+PACKAGE = None
+SERVICE = None
+
+# Server Configuration
+SERVER_TYPE = None
+PORT = 50051
+MAX_WORKERS = 100
+MAX_MESSAGE_LENGTH = 1024 * 1024 * 1024
+
+# gRPC Extension APIs
+EXTENSION_APIS = {
+    'spaceone.core.extension.grpc_health': ['GRPCHealth'],
+    'spaceone.core.extension.server_info': ['ServerInfo']
+}
