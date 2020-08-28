e888c1cf03fa8ca82cefa4d1b898f17949c2fc1f
Jongmin Kim <whdalsrnt@megazone.com>
2020-08-28 14:06:54 +0900

    Change a BaseModel interface
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/core/model/__init__.py
+++ src/spaceone/core/model/__init__.py
@@ -4,7 +4,6 @@ class BaseModel(object):
     def connect(cls):
         """ 
         Args:
-            None
         Returns:
             None
         """
@@ -14,25 +13,24 @@ class BaseModel(object):
     def create(cls, data):
         """ 
         Args:
-            data(dict)
+            data (dict)
         Returns:
-            model_vo(object)
+            model_vo (object)
         """
         raise NotImplementedError('model.create not implemented!')
 
     def update(self, data):
         """ 
         Args:
-            data(dict)
+            data (dict)
         Returns:
-            model_vo(object)
+            model_vo (object)
         """
         raise NotImplementedError('model.update not implemented!')
 
     def delete(self):
         """
         Args:
-            None
         Returns:
             None
         """
@@ -41,20 +39,39 @@ class BaseModel(object):
     def terminate(self):
         """ 
         Args:
-            None
         Returns:
             None
         """
         raise NotImplementedError('model.terminate not implemented!')
 
+    def increment(self, key, amount=1):
+        """
+        Args:
+            key (str)
+            amount (int)
+        Returns:
+            model_vo (object)
+        """
+        raise NotImplementedError('model.increment not implemented!')
+
+    def decrement(self, key, amount=1):
+        """
+        Args:
+            key (str)
+            amount (int)
+        Returns:
+            model_vo (object)
+        """
+        raise NotImplementedError('model.decrement not implemented!')
+
     @classmethod
     def get(cls, **conditions):
         """ 
         Args:
-            conditions(kwargs)
-            - key(str) : value(any)
+            **conditions (kwargs)
+            - key (str): value (any)
         Returns:
-            model_vo(object)
+            model_vo (object)
         """
         raise NotImplementedError('model.get not implemented!')
 
@@ -62,19 +79,18 @@ class BaseModel(object):
     def filter(cls, **conditions):
         """ 
         Args:
-            conditions(kwargs)
-            - key(str) : value(any)
+            **conditions (kwargs)
+            - key (str): value (any)
         Returns:
-            model_vos(list)
+            model_vos (list)
         """
         raise NotImplementedError('model.filter not implemented!')
 
     def to_dict(self):
         """ 
         Args:
-            None
         Returns:
-            model_data(dict)
+            model_data (dict)
         """
         raise NotImplementedError('model.to_dict not implemented!')
 
@@ -82,46 +98,50 @@ class BaseModel(object):
     def query(cls, **query):
         """
         Args:
-            query(kwargs)
-                - filter(list)
+            **query (kwargs)
+                - filter (list)
                 [
                     {
-                        'key' : 'field(str)', 
-                        'value' : 'value(any or list)',
-                        'operator' : 'lt | lte | gt | gte | eq | not | exists |
-                        contain | not_contain | in | not_in | not_contain_in | match | regex | regex_in'
+                        'key' : 'field (str)',
+                        'value' : 'value (any)',
+                        'operator' : 'lt | lte | gt | gte | eq | not | exists | contain |
+                        not_contain | in | not_in | not_contain_in | match | regex | regex_in |
+                        datetime_lt | datetime_lte | datetime_gt | datetime_gte |
+                        timediff_lt | timediff_lte | timediff_gt | timediff_gte'
                     },
                     ...
                 ]
-                - filter_or(list)
+                - filter_or (list)
                 [
                     {
-                        'key' : 'field(str)', 
-                        'value' : 'value(any or list)',
-                        'operator' : 'lt | lte | gt | gte | eq | not | exists |
-                        contain | not_contain | in | not_in | not_contain_in | match | regex | regex_in'
+                        'key' : 'field (str)',
+                        'value' : 'value (any)',
+                        'operator' : 'lt | lte | gt | gte | eq | not | exists | contain |
+                        not_contain | in | not_in | not_contain_in | match | regex | regex_in |
+                        datetime_lt | datetime_lte | datetime_gt | datetime_gte |
+                        timediff_lt | timediff_lte | timediff_gt | timediff_gte'
                     },
                     ...
                 ]
-                - sort(dict)
+                - sort (dict)
                 {
-                  'key' : 'field(str)',
+                  'key' : 'field (str)',
                   'desc' : True | False
                 }
-                - page(dict)
+                - page (dict)
                 {
-                    'start': (int),
-                    'limit' : (int)
+                    'start': 'start_row (int)',
+                    'limit' : 'row_limit (int)'
                 }
-                - distinct(str): 'field'
-                - only(list): ['field1', 'field2', '...']
+                - distinct (str): 'field'
+                - only (list): ['field1', 'field2', '...']
                 - exclude(list): ['field1', 'field2', '...']
-                - minimal(bool)
-                - count_only(bool)
+                - minimal (bool)
+                - count_only (bool)
 
         Returns:
-            model_vos(list)
-            total_count(int)
+            model_vos (list)
+            total_count (int)
         """
         raise NotImplementedError('model.query not implemented!')
 
@@ -129,24 +149,24 @@ class BaseModel(object):
     def stat(cls, **query):
         """
         Args:
-            query(kwargs)
-                - filter(list)
+            **query (kwargs)
+                - filter (list)
                 [
                     {
-                        'key' : 'field(str)',
-                        'value' : 'value(any or list)',
-                        'operator' : 'lt | lte | gt | gte | eq | not | exists |
-                        contain | not_contain | in | not_in | not_contain_in | match | regex | regex_in
-                        datetime_lt | datetime_lte | datetime_gt | datetime_gte | timediff_lt | timediff_lte
-                        timediff_gt | timediff_gte'
+                        'key' : 'field (str)',
+                        'value' : 'value (any)',
+                        'operator' : 'lt | lte | gt | gte | eq | not | exists | contain |
+                        not_contain | in | not_in | not_contain_in | match | regex | regex_in |
+                        datetime_lt | datetime_lte | datetime_gt | datetime_gte |
+                        timediff_lt | timediff_lte | timediff_gt | timediff_gte'
                     },
                     ...
                 ]
                 - filter_or(list)
                 [
                     {
-                        'key' : 'field(str)',
-                        'value' : 'value(any or list)',
+                        'key' : 'field (str)',
+                        'value' : 'value (any)',
                         'operator' : 'lt | lte | gt | gte | eq | not | exists |
                         contain | not_contain | in | not_in | not_contain_in | match | regex | regex_in
                         datetime_lt | datetime_lte | datetime_gt | datetime_gte | timediff_lt | timediff_lte
@@ -154,46 +174,46 @@ class BaseModel(object):
                     },
                     ...
                 ]
-                - aggregate(dict)
+                - aggregate (dict)
                 {
                     'unwind': [
                         {
-                            'path': 'key path(str)'
+                            'path': 'key path (str)'
                         }
                     ],
                     'group': {
                         'keys': [
                             {
-                                'key': 'field(str)',
-                                'name': 'alias name(str)'
+                                'key': 'field (str)',
+                                'name': 'alias name (str)'
                             },
                             ...
                         ],
                         'fields': [
                             {
-                                'key': 'field(str)',
-                                'name': 'alias name(str)',
+                                'key': 'field (str)',
+                                'name': 'alias name (str)',
                                 'operator': 'count | sum | avg | max | min | size | add_to_set | merge_objects'
                             },
                             ...
                         ]
                     }
                     'count': {
-                        'name': 'alias name(str)'
+                        'name': 'alias name (str)'
                     }
                 }
                 - sort(dict)
                 {
-                  'name' : 'field(str)',
+                  'name' : 'field (str)',
                   'desc' : True | False
                 }
                 - page(dict)
                 {
-                    'start': (int),
-                    'limit' : (int)
+                    'start': 'start_row (int)',
+                    'limit' : 'row_limit (int)'
                 }
 
         Returns:
-            values(list)
+            values (list)
         """
         raise NotImplementedError('model.stat not implemented!')
