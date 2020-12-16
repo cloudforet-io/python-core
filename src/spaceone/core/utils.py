8a63b0d5b326cab3df18090f1bb570420a270816
Jongmin Kim <whdalsrnt@megazone.com>
2020-12-16 19:55:08 +0900

    Refactor: get_dict_value utils
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/core/utils.py
+++ src/spaceone/core/utils.py
@@ -149,45 +149,6 @@ def deep_merge(from_dict: dict, into_dict: dict) -> dict:
     return into_dict
 
 
-def get_dict_value(data: dict, dotted_key: str, default_value=None):
-    try:
-        return reduce(dict.__getitem__, dotted_key.split('.'), data)
-    except Exception as e:
-        return default_value
-
-
-def change_dict_value(data: dict, dotted_key: str, change_value, change_type='value') -> dict:
-    # change_value = func or value(any type)
-    if '.' in dotted_key:
-        key, rest = dotted_key.split('.', 1)
-        if key in data:
-            if rest.startswith('[]') and isinstance(data[key], list):
-                list_data = []
-                for sub_data in data[key]:
-                    if rest.strip() == '[]':
-                        list_data.append(_change_value_by_type(change_type, sub_data, change_value))
-                    else:
-                        sub_rest = rest.split('.', 1)[1]
-                        list_data.append(change_dict_value(sub_data, sub_rest, change_value, change_type))
-                data[key] = list_data
-            elif isinstance(data[key], dict):
-                data[key] = change_dict_value(data[key], rest, change_value, change_type)
-    else:
-        if dotted_key in data:
-            data[dotted_key] = _change_value_by_type(change_type, data[dotted_key], change_value)
-
-    return data
-
-
-def _change_value_by_type(change_type, original_value, change_value):
-    if change_type == 'value':
-        return change_value
-    elif change_type == 'func':
-        return change_value(original_value)
-    else:
-        return original_value
-
-
 def parse_timediff_query(query: str) -> datetime:
     try:
         time_info, include_operator = _parse_timediff_from_regex(query)
@@ -245,6 +206,139 @@ def _parse_timediff_from_regex(query: str) -> Tuple[dict, bool]:
             raise ValueError(f'Timediff format is invalid. (value={query})')
 
         return {'base_time': query.strip()}, False
+
+
+def get_dict_value(data: dict, dotted_key: str, default_value=None):
+    if '.' in dotted_key:
+        key, rest = dotted_key.split(".", 1)
+
+        if isinstance(data, dict) and key in data:
+            if isinstance(data[key], list):
+                return get_list_values(data[key], rest, default_value)
+            else:
+                return get_dict_value(data[key], rest, default_value)
+
+        else:
+            return default_value
+    else:
+        if isinstance(data, dict):
+            return data.get(dotted_key)
+        else:
+            return default_value
+
+
+def get_list_values(values: list, dotted_key: str, default_value=None):
+    list_values = []
+    try:
+        # Get value by index
+        if '.' in dotted_key:
+            index, rest = dotted_key.split('.', 1)
+            index = int(index)
+            if index >= len(values):
+                return default_value
+            else:
+                list_values.append(values[index])
+        else:
+            return values[int(dotted_key)]
+
+    except Exception:
+        list_values = values
+        rest = dotted_key
+
+    # Check condition (cond_key:cond_value=>get_key)
+    match_option = 'contain'
+    if len(rest) > 1 and rest[0] == "$":
+        condition = True
+        try:
+            cond_option, rest = rest[1:].split('=>', 1)
+            cond_key, cond_value = cond_option.split(':')
+        except Exception as e:
+            # Syntax Error
+            return default_value
+
+        if cond_value[:1] == '=':
+            match_option = 'eq'
+            cond_value = cond_value[1:]
+
+        elif cond_value[:1] == '!':
+            match_option = 'not'
+            cond_value = cond_value[1:]
+
+    else:
+        condition = False
+        cond_key = None
+        cond_value = None
+
+    results = []
+    for value in list_values:
+        # Get value from condition
+        if condition and not _check_condition(match_option, value[cond_key], cond_value):
+            continue
+
+        # Get value from dict key
+        result = get_dict_value(value, rest)
+
+        if result:
+            if isinstance(result, list):
+                results += result
+            else:
+                results.append(result)
+
+    try:
+        return list(set(results))
+    except Exception:
+        return results
+
+
+def _check_condition(match_option: str, val1, val2):
+    val1 = str(val1).lower()
+    val2 = str(val2).lower()
+
+    if match_option == 'eq':
+        if val1 == val2:
+            return True
+
+    elif match_option == 'not':
+        if val1.find(val2) < 0:
+            return True
+
+    else:
+        if val1.find(val2) >= 0:
+            return True
+
+    return False
+
+
+def change_dict_value(data: dict, dotted_key: str, change_value, change_type='value') -> dict:
+    # change_value = func or value(any type)
+    if '.' in dotted_key:
+        key, rest = dotted_key.split('.', 1)
+        if key in data:
+            if rest.startswith('[]') and isinstance(data[key], list):
+                list_data = []
+                for sub_data in data[key]:
+                    if rest.strip() == '[]':
+                        list_data.append(_change_value_by_type(change_type, sub_data, change_value))
+                    else:
+                        sub_rest = rest.split('.', 1)[1]
+                        list_data.append(change_dict_value(sub_data, sub_rest, change_value, change_type))
+                data[key] = list_data
+            elif isinstance(data[key], dict):
+                data[key] = change_dict_value(data[key], rest, change_value, change_type)
+    else:
+        if dotted_key in data:
+            data[dotted_key] = _change_value_by_type(change_type, data[dotted_key], change_value)
+
+    return data
+
+
+def _change_value_by_type(change_type, original_value, change_value):
+    if change_type == 'value':
+        return change_value
+    elif change_type == 'func':
+        return change_value(original_value)
+    else:
+        return original_value
 
 
 if __name__ == '__main__':
