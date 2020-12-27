8796c5525503394c585814385b6daf349306fafb
Jongmin Kim <whdalsrnt@megazone.com>
2020-12-28 04:32:44 +0900

    Change mutation handler using metadata
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/core/handler/mutation_handler.py
+++ src/spaceone/core/handler/mutation_handler.py
@@ -9,14 +9,14 @@ class SpaceONEMutationHandler(BaseMutationHandler):
     def request(self, params):
         role_type = self.transaction.get_meta('auth.role_type')
         domain_id = self.transaction.get_meta('domain_id')
-        project_role_inject_params = self.transaction.get_meta('auth.project.inject', {})
+        inject_filter = self.transaction.get_meta('auth.inject_filter', {})
 
         if role_type in ['DOMAIN', 'PROJECT']:
             params['domain_id'] = domain_id
 
         if role_type == 'PROJECT':
-            if isinstance(project_role_inject_params, dict):
-                for key, value in project_role_inject_params.items():
+            if isinstance(inject_filter, dict):
+                for key, value in inject_filter.items():
                     if key not in params:
                         params[key] = self.transaction.get_meta(value)
 
