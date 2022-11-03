7ec491a7083105b24b869c3f73aba136594848fd
Jongmin Kim <whdalsrnt@megazone.com>
2022-11-03 21:55:12 +0900

    refactor: refactor rest server logic

--- src/spaceone/core/fastapi/server.py
+++ src/spaceone/core/fastapi/server.py
@@ -1,18 +1,81 @@
 import logging
 import uvicorn
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from spaceone.core import config
 from spaceone.core.logger import set_logger
+from spaceone.core.extension.server_info import ServerInfoManager
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def api_app():
-    conf = config.get_global()
-    package = conf['PACKAGE']
+def _get_router_conf(package):
+    router_conf_module = __import__(f'{package}.conf.router_conf', fromlist=['router_conf'])
+    return getattr(router_conf_module, 'ROUTER', [])
 
-    rest_route_module = __import__(f'{package}.interface.rest.router', fromlist=['router'])
-    return getattr(rest_route_module, 'app', {})
+
+def _get_router(path):
+    try:
+        module_path, router_name = path.split(':')
+        router_module = __import__(module_path, fromlist=[module_path[-1:]])
+        return router_module.getattr(router_module, router_name)
+    except Exception as e:
+        _LOGGER.warning(f'[_get_router] Invalid router path. (router_path = {path})')
+
+
+def include_routers(app):
+    # Include All Routers from router_conf.py
+    routers = _get_router_conf(config.get_package())
+
+    all_routers_path = []
+    for router_options in routers:
+        if 'path' in router_options:
+            all_routers_path.append(router_options['path'])
+            router = _get_router(router_options['path'])
+            del router_options['path']
+
+            app.include_router(
+                router.router,
+                **router_options
+            )
+        else:
+            _LOGGER.warning(f'[_include_router] Undefined router path. (router = {router_options})')
+
+    all_routers_path_str = '\n\t - '.join(all_routers_path)
+    _LOGGER.debug(f'Loaded Routers: \n\t - {all_routers_path_str}')
+
+    return app
+
+
+def add_middlewares(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+    return app
+
+
+def init_fast_api():
+    global_conf = config.get_global()
+    server_info = ServerInfoManager()
+    return FastAPI(
+        title=global_conf.get('TITLE'),
+        version=server_info.get_version(),
+        contact=global_conf.get('CONTACT'),
+        description=global_conf.get('DESCRIPTION'),
+    )
+
+
+def fast_api_app():
+    app = init_fast_api()
+    app = add_middlewares(app)
+    app = include_routers(app)
+    return app
 
 
 def serve():
@@ -22,7 +85,9 @@ def serve():
     if conf.get('SET_LOGGING', True):
         set_logger()
 
-    _LOGGER.info(f'Start REST Server ({config.get_service()}): '
-                 f'host={conf["HOST"]} port={conf["PORT"]}')
+    uvicorn_options = conf.get('UVICORN_OPTIONS', {})
 
-    uvicorn.run('spaceone.core.fastapi.server:api_app', host=conf['HOST'], port=conf['PORT'], factory=True)
+    _LOGGER.info(f'Start REST Server ({config.get_service()}): '
+                 f'host={conf["HOST"]} port={conf["PORT"]} options={uvicorn_options}')
+
+    uvicorn.run('spaceone.core.fastapi.server:fast_api_app', host=conf['HOST'], port=conf['PORT'], **uvicorn_options)
