456fe0344874813dffc3ad008a6718ea04326075
Jongmin Kim <whdalsrnt@megazone.com>
2023-10-27 18:54:02 +0900

    refactor: remove unused func

--- src/spaceone/core/pygrpc/api.py
+++ src/spaceone/core/pygrpc/api.py
@@ -57,10 +57,6 @@ class BaseAPI(object):
                 setattr(self, f_name, self._grpc_method(f_object, config.get_service()))
 
     @staticmethod
-    def get_minimal(params):
-        return params.get('query', {}).get('minimal', False)
-
-    @staticmethod
     def _error_method(error, context):
         if not isinstance(error, ERROR_BASE):
             error = ERROR_UNKNOWN(message=error)
