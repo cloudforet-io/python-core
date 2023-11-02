c4f3aacf81de7881548009d20e06b63308e3b0ad
Jongmin Kim <whdalsrnt@megazone.com>
2023-11-02 22:10:30 +0900

    refactor: refactor event handler

--- src/spaceone/core/handler/event_handler.py
+++ src/spaceone/core/handler/event_handler.py
@@ -1,6 +1,5 @@
 import logging
 
-from spaceone.core import pygrpc
 from spaceone.core import utils
 from spaceone.core.handler import BaseEventHandler
 from spaceone.core.error import ERROR_HANDLER_CONFIGURATION
@@ -19,18 +18,7 @@ class EventGRPCHandler(BaseEventHandler):
             _LOGGER.error(f'[_initialize] uri config is undefined.')
             raise ERROR_HANDLER_CONFIGURATION(handler='AuthenticationGRPCHandler')
 
-        try:
-            self.uri_info = utils.parse_grpc_uri(self.config['uri'])
-        except Exception as e:
-            _LOGGER.error(f'[_initialize] AuthenticationGRPCHandler Init Error: {e}')
-            raise ERROR_HANDLER_CONFIGURATION(handler='AuthenticationGRPCHandler')
+        self.client: SpaceConnector = self.locator.get_connector(SpaceConnector, endpoint=self.config['uri'])
 
     def notify(self, status: str, message: dict):
-        grpc_method = pygrpc.get_grpc_method(self.uri_info)
-        grpc_method({
-            'service': self.transaction.service,
-            'resource': self.transaction.resource,
-            'verb': self.transaction.verb,
-            'status': status,
-            'message': message
-        })
+        _LOGGER.debug(f'[notify] {status}: {message}')
