f52c3f90984c3cb0277d4ba3734352bb44241023
Jongmin Kim <whdalsrnt@megazone.com>
2023-11-02 22:10:50 +0900

    feat: add plugin config

---
+++ src/spaceone/core/plugin/plugin_conf.py
@@ -0,0 +1,9 @@
+PLUGIN_SOURCES = {
+    'auth': 'spaceone.identity.plugin.auth.skeleton',
+    'asset': 'spaceone.inventory.plugin.collector.skeleton',
+    'metric': 'spaceone.monitoring.plugin.metric.skeleton',
+    'log': 'spaceone.monitoring.plugin.log.skeleton',
+    'webhook': 'spaceone.monitoring.plugin.webhook.skeleton',
+    'cost': 'spaceone.cost_analysis.plugin.data_source.skeleton',
+    'notification': 'spaceone.notification.plugin.protocol.skeleton',
+}
