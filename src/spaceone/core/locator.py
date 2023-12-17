11143db870e0649bb0fe1e6df6088a10cbad294f
Jongmin Kim <whdalsrnt@megazone.com>
2023-12-17 22:58:12 +0900

    refactor: change backend settings in connector

--- src/spaceone/core/locator.py
+++ src/spaceone/core/locator.py
@@ -7,7 +7,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def _get_module(package, target):
-    return __import__(f'{package}.{target}', fromlist=[f'{target}'])
+    return __import__(f"{package}.{target}", fromlist=[f"{target}"])
 
 
 class Locator(object):
@@ -17,7 +17,7 @@ class Locator(object):
         package = config.get_package()
 
         if isinstance(name_or_object, str):
-            service_module = _get_module(package, 'service')
+            service_module = _get_module(package, "service")
             return getattr(service_module, name_or_object)(metadata=metadata, **kwargs)
         else:
             return name_or_object(metadata=metadata, **kwargs)
@@ -27,7 +27,7 @@ class Locator(object):
         package = config.get_package()
         try:
             if isinstance(name_or_object, str):
-                manager_module = _get_module(package, 'manager')
+                manager_module = _get_module(package, "manager")
                 return getattr(manager_module, name_or_object)(**kwargs)
             else:
                 return name_or_object(**kwargs)
@@ -36,14 +36,16 @@ class Locator(object):
             raise e
 
         except Exception as e:
-            raise ERROR_LOCATOR(name=name_or_object, reason=e, _meta={'type': 'manager'})
+            raise ERROR_LOCATOR(
+                name=name_or_object, reason=e, _meta={"type": "manager"}
+            )
 
     @staticmethod
     def get_info(name_or_object: [str, object], *args, **kwargs):
         package = config.get_package()
         try:
             if isinstance(name_or_object, str):
-                info_module = _get_module(package, 'info')
+                info_module = _get_module(package, "info")
                 return getattr(info_module, name_or_object)(*args, **kwargs)
             else:
                 return name_or_object(*args, **kwargs)
@@ -52,14 +54,14 @@ class Locator(object):
             raise e
 
         except Exception as e:
-            raise ERROR_LOCATOR(name=name_or_object, reason=e, _meta={'type': 'info'})
+            raise ERROR_LOCATOR(name=name_or_object, reason=e, _meta={"type": "info"})
 
     @staticmethod
     def get_model(name_or_object: [str, object]):
         package = config.get_package()
         try:
             if isinstance(name_or_object, str):
-                model_module = _get_module(package, 'model')
+                model_module = _get_module(package, "model")
                 return getattr(model_module, name_or_object)
             else:
                 return name_or_object
@@ -68,7 +70,9 @@ class Locator(object):
             raise e
 
         except Exception as e:
-            raise ERROR_LOCATOR(name=f'{name_or_object} Model', reason=e, _meta={'type': 'model'})
+            raise ERROR_LOCATOR(
+                name=f"{name_or_object} Model", reason=e, _meta={"type": "model"}
+            )
 
     @staticmethod
     def get_connector(name_or_object: [str, object], **kwargs):
@@ -77,16 +81,20 @@ class Locator(object):
         try:
             if isinstance(name_or_object, str):
                 connector_conf = config.get_connector(name_or_object)
-                backend = connector_conf.get('backend')
+                backend = connector_conf.get("backend")
 
                 if backend:
-                    connector_module_path, connector_name = backend.rsplit('.', 1)
-                    connector_module = __import__(connector_module_path, fromlist=[connector_name])
+                    connector_module_path, connector_name = backend.rsplit(":", 1)
+                    connector_module = __import__(
+                        connector_module_path, fromlist=[connector_name]
+                    )
                 else:
-                    connector_module = _get_module(package, 'connector')
+                    connector_module = _get_module(package, "connector")
                     connector_name = name_or_object
 
-                return getattr(connector_module, connector_name)(connector_name=name_or_object, **kwargs)
+                return getattr(connector_module, connector_name)(
+                    connector_name=name_or_object, **kwargs
+                )
             else:
                 return name_or_object(**kwargs)
 
@@ -94,4 +102,6 @@ class Locator(object):
             raise e
 
         except Exception as e:
-            raise ERROR_LOCATOR(name=name_or_object, reason=e, _meta={'type': 'connector'})
+            raise ERROR_LOCATOR(
+                name=name_or_object, reason=e, _meta={"type": "connector"}
+            )
