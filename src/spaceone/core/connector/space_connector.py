0245429981a96119a0984e4365f026ab0d0df71a
Jongmin Kim <whdalsrnt@megazone.com>
2023-12-19 00:20:02 +0900

    fix: set space_connector name for configuration

--- src/spaceone/core/connector/space_connector.py
+++ src/spaceone/core/connector/space_connector.py
@@ -16,15 +16,16 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class SpaceConnector(BaseConnector):
+    name = "SpaceConnector"
 
     def __init__(
-            self,
-            *args,
-            service: str = None,
-            endpoint: str = None,
-            token: str = None,
-            return_type: str = 'dict',
-            **kwargs
+        self,
+        *args,
+        service: str = None,
+        endpoint: str = None,
+        token: str = None,
+        return_type: str = "dict",
+        **kwargs,
     ):
         super().__init__(*args, **kwargs)
         self._service = service
@@ -33,7 +34,7 @@ class SpaceConnector(BaseConnector):
         self._return_type = return_type
 
         self._client = None
-        self._endpoints: dict = self.config.get('endpoints', {})
+        self._endpoints: dict = self.config.get("endpoints", {})
 
         self._verify()
         self._init_client()
@@ -52,9 +53,11 @@ class SpaceConnector(BaseConnector):
         params = params or {}
         metadata = self._get_connection_metadata(token)
 
-        response_or_iterator = getattr(getattr(self._client, resource), verb)(params, metadata=metadata)
+        response_or_iterator = getattr(getattr(self._client, resource), verb)(
+            params, metadata=metadata
+        )
 
-        if self._return_type == 'dict':
+        if self._return_type == "dict":
             if isinstance(response_or_iterator, types.GeneratorType):
                 return self._generate_response(response_or_iterator)
             else:
@@ -65,19 +68,26 @@ class SpaceConnector(BaseConnector):
     def _verify(self) -> None:
         if self._service:
             if not isinstance(self._endpoints, dict):
-                raise ERROR_CONNECTOR_CONFIGURATION(connector='SpaceConnector')
+                raise ERROR_CONNECTOR_CONFIGURATION(connector="SpaceConnector")
 
             if self._service not in self._endpoints:
-                raise ERROR_CONNECTOR_LOAD(connector='SpaceConnector', reason=f'{self._service} endpoint is undefined.')
+                raise ERROR_CONNECTOR_LOAD(
+                    connector="SpaceConnector",
+                    reason=f"{self._service} endpoint is undefined.",
+                )
 
         elif not self._endpoint:
-            raise ERROR_CONNECTOR_LOAD(connector='SpaceConnector', reason='service or endpoint is required.')
+            raise ERROR_CONNECTOR_LOAD(
+                connector="SpaceConnector", reason="service or endpoint is required."
+            )
 
     def _init_client(self) -> None:
         endpoint = self._get_endpoint()
         e = parse_grpc_endpoint(endpoint)
         self._client: GRPCClient = pygrpc.client(
-            endpoint=e['endpoint'], ssl_enabled=e['ssl_enabled'], max_message_length=1024 * 1024 * 256
+            endpoint=e["endpoint"],
+            ssl_enabled=e["ssl_enabled"],
+            max_message_length=1024 * 1024 * 256,
         )
 
     @staticmethod
@@ -92,26 +102,28 @@ class SpaceConnector(BaseConnector):
         metadata = []
 
         if token:
-            metadata.append(('token', token))
+            metadata.append(("token", token))
         elif self._token:
-            metadata.append(('token', self._token))
-        elif token := self.transaction.meta.get('token'):
-            metadata.append(('token', token))
+            metadata.append(("token", self._token))
+        elif token := self.transaction.meta.get("token"):
+            metadata.append(("token", token))
 
         carrier = {}
         TraceContextTextMapPropagator().inject(carrier)
 
-        if traceparent := carrier.get('traceparent'):
-            metadata.append(('traceparent', traceparent))
+        if traceparent := carrier.get("traceparent"):
+            metadata.append(("traceparent", traceparent))
 
         return metadata
 
     def _parse_method(self, method: str) -> Tuple[str, str]:
         try:
-            resource, verb = method.split('.')
+            resource, verb = method.split(".")
         except Exception:
-            raise ERROR_CONNECTOR(connector='SpaceConnector',
-                                  reason=f'Method is invalid. (endpoint = {self._get_endpoint()}, method = {method})')
+            raise ERROR_CONNECTOR(
+                connector="SpaceConnector",
+                reason=f"Method is invalid. (endpoint = {self._get_endpoint()}, method = {method})",
+            )
 
         return resource, verb
 
@@ -120,9 +132,9 @@ class SpaceConnector(BaseConnector):
 
         if supported_verb is None or verb not in supported_verb:
             raise ERROR_CONNECTOR(
-                connector='SpaceConnector',
-                reason=f'Method not supported. '
-                       f'(endpoint = {self._get_endpoint()}, method = {resource}.{verb})'
+                connector="SpaceConnector",
+                reason=f"Method not supported. "
+                f"(endpoint = {self._get_endpoint()}, method = {resource}.{verb})",
             )
 
     def _get_endpoint(self) -> str:
