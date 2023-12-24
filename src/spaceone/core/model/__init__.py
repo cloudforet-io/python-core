1945fce9a05cde57928c5f380613a7c53d98b4e3
Jongmin Kim <whdalsrnt@megazone.com>
2023-12-24 14:50:06 +0900

    fix: fix authorization bug

--- src/spaceone/core/model/__init__.py
+++ src/spaceone/core/model/__init__.py
@@ -1,5 +1,5 @@
 from spaceone.core.model.mongo_model import MongoModel
 
 
-def init_all():
-    MongoModel.init()
+def init_all(create_index: bool = True) -> None:
+    MongoModel.init(create_index)
